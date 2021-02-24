ff24f89439e2b0269f417252ee80a53954872b39
Mohammed saif <saifchohan7235@gmail.com>
2021-02-24 17:43:28 +0530

    api

---
+++ app/Http/Controllers/Api/NewsfeedController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class NewsfeedController extends Controller
+{
+    public function index()
+    {
+
+    }
+}
