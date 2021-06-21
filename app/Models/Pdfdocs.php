ec4594024dd8d64989bf15c1f03bdcccc86507cd
wasimcs96 <wasimcs96@gmail.com>
2021-06-21 10:33:31 +0000

    DFD

---
+++ app/Models/Pdfdocs.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pdfdocs extends Model
+{
+    protected $table = 'pdf_docs';
+
+    protected $fillable = ['file','file_name'];
+}
